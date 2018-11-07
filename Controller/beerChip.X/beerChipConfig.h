d346a4f396000e3f5987b92cc56e871899300afa
Blessh2o <you@example.com>
2018-11-06 22:52:24 -0500

    Make build

--- Controller/beerChip.X/beerChipConfig.h
+++ Controller/beerChip.X/beerChipConfig.h
@@ -18,7 +18,7 @@ extern "C" {
 #define BEERCHIP_PRODUCT_ID    0x0B
 #define BEERCHIP_MAJOR_VERSION 0
 #define BEERCHIP_MINOR_VERSION 0
-#define BEERCHIP_BUILD         9
+#define BEERCHIP_BUILD         0x0A
 
 /*
 ** Different clock values for OSCCON
