61f48ee0e877e6647c1112e813cfb127cae9c3bc
root <bob@fdsdf.com>
2019-01-19 00:18:45 -0500

    Version 4

--- DataLogger/beermonConfig.py
+++ DataLogger/beermonConfig.py
@@ -1,7 +1,7 @@
 beermonConfig = {
     'majVersion' : 0,
     'minVersion' : 0,
-    'buildVersion' : 3,
+    'buildVersion' : 4,
     'dbLocation' : '/opt/beermon/data/beermonSQLite3DB.db',
     'dbSessionLocation' : '/opt/beermon/data/beermonSession.db',
     'i2cBus' : 1,
