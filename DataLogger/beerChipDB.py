b51ef7db8588e87368d5ab91a504eab797a3bb36
blesswater <blessh2o@gmail.com>
2019-10-19 19:01:49 -0700

    Working query Temp

--- DataLogger/beerChipDB.py
+++ DataLogger/beerChipDB.py
@@ -182,9 +182,15 @@ class beerChipSQLiteDB( beerChipDB ):
             prbSql += "GROUP BY strftime('%%s', temp_time) / %d" % interval
             probeUnion.append( prbSql )
         sql  = "SELECT x.time_group, "
-        sql += "DATETIME(AVG(STRFTIME('%s',x.centerTime)),'unixepoch'), "
-        sql += [x for x in probes[1]]
+        sql += "DATETIME(AVG(STRFTIME('%%s',x.centerTime)),'unixepoch'), "
+        sql += ', '.join([str('MAX(x.' + x[1] + ')') for x in probes]) + ' '
+        sql += "FROM ( "
+        sql += " UNION ".join(probeUnion)
+        sql += ") x "
+        sql += "GROUP BY x.time_group"
 
+        for row in self.query( sql ):
+            yield row
 
     def close(self):
         if( self.conn != None ):
